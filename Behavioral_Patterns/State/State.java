d56dcadab095983040054a0b53919bd1503eb51a
adrixo <adrianvalrom@gmail.com>
2019-01-31 01:01:10 +0100

    ultimo

---
+++ Behavioral_Patterns/State/State.java
@@ -0,0 +1,61 @@
+//The state pattern is used in computer programming to encapsulate varying behavior for the same object, based on its internal state. This can be a cleaner way for an object to change its behavior at runtime without resorting to conditional statements and thus improve maintainability.
+
+interface Statelike {
+  void writeName(StateContext context, String name);
+}
+
+class StateLowerCase implements Statelike {
+  @Override
+  public void writeName(final StateContext context, final String name) {
+    System.out.println(name.toLowerCase());
+    context.setState(new StateMultipleUpperCase());
+  }
+}
+
+class StateMultipleUpperCase implements Statelike {
+  /* Counter local to this state */
+  private int count = 0;
+
+  @Override
+  public void writeName(final StateContext context, final String name) {
+    System.out.println(name.toUpperCase());
+    /* Change state after StateMultipleUpperCase's writeName() gets invoked twice */
+    if(++count > 1) {
+      context.setState(new StateLowerCase());
+    }
+  }
+}
+
+class StateContext {
+  private Statelike myState;
+  StateContext() {
+    setState(new StateLowerCase());
+  }
+
+  /**
+   * Setter method for the state.
+   * Normally only called by classes implementing the State interface.
+   * @param newState the new state of this context
+   */
+  void setState(final Statelike newState) {
+    myState = newState;
+  }
+
+  public void writeName(final String name) {
+    myState.writeName(this, name);
+  }
+}
+
+public class DemoOfClientState {
+  public static void main(String[] args) {
+    final StateContext sc = new StateContext();
+
+    sc.writeName("Monday");
+    sc.writeName("Tuesday");
+    sc.writeName("Wednesday");
+    sc.writeName("Thursday");
+    sc.writeName("Friday");
+    sc.writeName("Saturday");
+    sc.writeName("Sunday");
+  }
+}
