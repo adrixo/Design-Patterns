bbabecce7ecc3f12b188fb8cb332992cf3b492fd
adrixo <adrianvalrom@gmail.com>
2019-02-09 09:21:35 +0100

    specification

---
+++ Behavioral_Patterns/Specification/Specification.java
@@ -0,0 +1,1 @@
+//In computer programming, the specification pattern is a particular software design pattern, whereby business rules can be recombined by chaining the business rules together using boolean logic. The pattern is frequently used in the context of domain-driven design.
