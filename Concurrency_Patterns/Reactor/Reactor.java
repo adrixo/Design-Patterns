156676abcfc040aed14812e134838476b48dd5c4
adrixo <adrianvalrom@gmail.com>
2019-02-14 00:21:24 +0100

    lasts

---
+++ Concurrency_Patterns/Reactor/Reactor.java
@@ -0,0 +1,1 @@
+//The reactor design pattern is an event handling pattern for handling service requests delivered concurrently to a service handler by one or more inputs. The service handler then demultiplexes the incoming requests and dispatches them synchronously to the associated request handlers.
